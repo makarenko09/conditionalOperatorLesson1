2b3c49d3f25b007b54ece0b6a5a66d40a7a71def
avare41lll <konmakarenko@outlook.com>
2024-11-09 16:40:27 +0300

    execute all tasks of lesson two

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.time.Month;
+
 public class Main {
     public static void main(String[] args) {
 //task 1
@@ -25,13 +27,13 @@ public class Main {
         }
         //task 4
         byte oldPerson = 18;
-        if (oldPerson >= 2 && oldPerson <= 6) {
+        if (oldPerson >= 2 && oldPerson < 6) {
             System.out.println("Если возраст человека равен " + oldPerson + "+, то ему нужно ходить в детский сад.");
-        } else if (oldPerson >= 7 && oldPerson <= 18) {
+        } else if (oldPerson >= 6 && oldPerson < 18) {
             System.out.println("Если возраст человека равен " + oldPerson + ", то ему нужно ходить в школу.");
-        } else if (oldPerson >= 19 && oldPerson <= 24) {
-            System.out.println("Если возраст человека равен " + oldPerson + "+,  то его место в университете.");
-        } else if (oldPerson >= 25) {
+        } else if (oldPerson >= 18 && oldPerson < 24) {
+            System.out.println("Если возраст человека равен " + oldPerson + ",  то его место в университете.");
+        } else if (oldPerson >= 24) {
             System.out.println("Если возраст человека равен " + oldPerson + ",  то ему пора ходить на работу");
         }
         //task 5
@@ -71,4 +73,93 @@ public class Main {
         }
         System.out.print("Largest number: " + largest);
         // А можно было бы использовать пузырьковую сортировку...
-    }}
+        //task 1 lesson 2
+        byte clientOS;
+        String oc = "Android";
+        switch (oc) {
+            case "iOS":
+                clientOS = 0;
+                System.out.println("Установите версию приложения для iOS по ссылке");
+                break;
+            case "Android":
+                clientOS = 1;
+                System.out.println("Установите версию приложения для Android по ссылке");
+                break;
+        }
+        //task 2 lesson 2
+        short clientDeviceYear = 2015;
+        String warning = clientDeviceYear < 2015 ? "облегченную" : "обычную";
+        byte clientOSf;
+
+        String Oc = "Android";
+        switch (Oc) {
+            case "iOS":
+                clientOSf = 0;
+                System.out.println("Установите " + warning + " версию приложения для iOS по ссылке");
+                break;
+            case "Android":
+                clientOSf = 1;
+                System.out.println("Установите " + warning + " версию приложения для Android по ссылке");
+                break;
+        }
+        //task 3 lesson 2
+        short changeYear = 1552;
+        short startLeapYear = 1548;
+        if (changeYear >= startLeapYear) {
+            if (changeYear % 4 == 0) {
+                System.out.println("Год " + changeYear + " является високосным");
+            } else if (changeYear % 400 == 0) {
+                System.out.println("Год " + changeYear + " является високосным");
+            } else {
+                System.out.println("Год " + changeYear + " не является високосным");
+            }
+        } else {
+            System.out.println("Год " + changeYear + " не является високосным");
+        }
+        //task 4 lesson 2
+        short radiusMax = 100;
+        short deliveryDistance = 95;
+
+        if (deliveryDistance < radiusMax && deliveryDistance > 0) {
+            System.out.print("Потребуется дней: ");
+            if (deliveryDistance < 20) {
+                System.out.print("одни сутки");
+            } else if (deliveryDistance < 60) {
+                System.out.print("двое суток");
+            } else if (deliveryDistance < 100) {
+                System.out.print("трое суток");
+            }
+        }
+        //task 5 lesson 2
+        int monthNumber = 6;
+        System.out.println();
+        /*Прописываем условие в котором программа не будет выполняться*/
+        if (monthNumber < 1 || monthNumber > 12) {
+            System.out.println("Номер месяца должен быть от 1 до 12");
+        }
+
+        Month month = Month.of(monthNumber);
+        switch (month) {
+            case DECEMBER:
+            case JANUARY:
+            case FEBRUARY:
+                System.out.println("Зима");
+                break;
+            case MARCH:
+            case APRIL:
+            case MAY:
+                System.out.println("Весна");
+                break;
+            case JUNE:
+            case JULY:
+            case AUGUST:
+                System.out.println("Лето");
+                break;
+            case SEPTEMBER:
+            case OCTOBER:
+            case NOVEMBER:
+                System.out.println("Осень");
+                break;
+        }
+    }
+}
